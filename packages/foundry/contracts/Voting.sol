1379b5e816f5ee107232f42eb06e4362ade78b65
owlwilderness.eth <98717833+OwlWilderness@users.noreply.github.com>
2025-08-24 11:58:53 -0400

    step1

--- packages/foundry/contracts/Voting.sol
+++ packages/foundry/contracts/Voting.sol
@@ -3,5 +3,17 @@
 pragma solidity ^0.8.19;
 
 import { console2 } from "../lib/forge-std/src/console2.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+contract votekh{
+    IERC20 public drToken;          //decentralized resistance token
+    uint256 public voteOpenPeriod;  //time period vote is open
+
+    constructor(IERC20 _drTokenAddress, uint256 _voteOpenPeriod){
+        drToken = IERC20(_drTokenAddress);
+        voteOpenPeriod = _voteOpenPeriod;
+    }
+
+
+}
 // Good luck!
